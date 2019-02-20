13e91bab91650984605bd520fbbd1f280d5608b5
iancupgop <ian18charles@gmail.com>
2019-02-20 09:06:39 -0600

    ViewController 1st v
    ViewController Xcode

---
+++ ianCarlos1402019StoryBoard/ianCarlos1402019StoryBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ianCarlos1402019StoryBoard
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 14/02/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
